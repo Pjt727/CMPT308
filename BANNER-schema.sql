060fc90b562a836a7a031b391ca34897cd4d51bb
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-04-16 23:45:36 -0400

    Final Project: Work on schema and start on lang
    Also super secret file that totally doesnt have the password to the
    databases on my local machine

--- BANNER-schema.sql
+++ BANNER-schema.sql
@@ -1,50 +1,61 @@
-DROP TABLE IF EXISTS Professor;
+DROP TABLE IF EXISTS Meetings;
+DROP TABLE IF EXISTS Sections;
+DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Subjects;
+DROP TABLE IF EXISTS Schools;
+DROP TABLE IF EXISTS Professors;
+
 DROP TYPE IF EXISTS dayOfWeek;
-CREATE TYPE dayOfWeek AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday ', 'Sunday');
+CREATE TYPE dayOfWeek AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 
 CREATE TABLE Professors (
     id SERIAL PRIMARY KEY,
     emailAddress text,
-    bannerId text, -- Never trust others
+    bannerId text UNIQUE , -- Never trust others
     firstName text,
-    lastName text,
-    unique(emailAddress)
+    lastName text
 );
 
 CREATE TABLE Schools (
     code char(2) PRIMARY KEY,
-    name text,
+    name text
 );
 
-CREATE TABLE Subject (
+CREATE TABLE Subjects (
     code char(4) PRIMARY KEY,
     name text,
-    schoolCode char(2) REFERENCES Schools(code),
+    schoolCode char(2) REFERENCES Schools(code)
 );
 
 CREATE TABLE Courses (
-    number char(4) PRIMARY KEY,
-    subjectCode char(4) PRIMARY KEY REFERENCES Subject(code),
-    bannerId text, -- Never trust others
-    name text, 
+    number char(4),
+    subjectCode char(4) REFERENCES Subjects(code),
+    bannerId text UNIQUE,-- Never trust others
+    name text,
+    PRIMARY KEY(number, subjectCode)
 );
 
 CREATE TABLE Sections (
-    courseNumber char(4) PRIMARY KEY REFERENCES Courses(number),
-    subjectCode char(4) PRIMARY KEY REFERENCES Courses(code),
-    number char(4) PRIMARY KEY,
-    term text PRIMARY KEY, 
-    bannerId text, -- Never trust others
-    primaryProfessor int REFERENCES Professor(id),
+    courseNumber char(4),
+    subjectCode char(4),
+    number char(4),
+    term text, 
+    bannerId text UNIQUE, -- Never trust others
+    primaryProfessor int REFERENCES Professors(id),
+    FOREIGN KEY (courseNumber, subjectCode) REFERENCES Courses(number, subjectCode),
+    PRIMARY KEY(courseNumber, subjectCode, number, term)
 );
 
 CREATE TABLE Meetings (
-    courseNumber char(4) PRIMARY KEY REFERENCES Sections(courseNumber),
-    subjectCode char(4) PRIMARY KEY REFERENCES Sections(subjectCode),
-    sectionNumber char(4) PRIMARY KEY REFERENCES Sections(number),
-    term text PRIMARY KEY REFERENCES Sections(term), 
-    startTime TIME PRIMARY KEY,
-    day dayOfWeek PRIMARY KEY,
+    courseNumber char(4),
+    subjectCode char(4),
+    sectionNumber char(4),
+    term text, 
+    startTime TIME,
+    day dayOfWeek,
     duration INTERVAL,
+    FOREIGN KEY (courseNumber, subjectCode, sectionNumber, term) 
+        REFERENCES Sections(courseNumber, subjectCode, number, term),
+    PRIMARY KEY(courseNumber, subjectCode, sectionNumber, term, startTime, day)
 );
 
